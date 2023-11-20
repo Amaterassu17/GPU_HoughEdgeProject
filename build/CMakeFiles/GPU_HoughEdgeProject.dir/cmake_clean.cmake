be40ebdb6f4bf8b0fae95325c6f877642965492b
Amaterassu17 <gennusoangelo17@gmail.com>
2023-11-20 15:04:36 +0100

    CmMake Commit

---
+++ build/CMakeFiles/GPU_HoughEdgeProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/GPU_HoughEdgeProject"
+  "../bin/GPU_HoughEdgeProject.pdb"
+  "CMakeFiles/GPU_HoughEdgeProject.dir/main.cpp.o"
+  "CMakeFiles/GPU_HoughEdgeProject.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GPU_HoughEdgeProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
