8523a964fe0ae7c6a2638878102f0af1983b4de5
amaterassu <gennusoangelo17@gmail.com>
2023-11-20 09:29:10 +0100

    Initial Commit. Commited Libraries and empty main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Angelo Gennuso on 20/11/23.
+//
+
+#include <chrono>
+#include <cmath>
+#include <iostream>
+
+//#include "libs/args.hxx" //optional library for parsing command line options
+
+#include "libs/matrix_utils.h"
+//this file includes the functions:
+/*
+ * fill_random
+ * fill_zero
+ * load_matrix
+ * print_matrix
+ * check_matrix (computation of C= A*B)
+ */
+
+#ifdef DP
+typedef double REAL;
+#else
+typedef float REAL;
+#endif
+#define check_out 1
+
+
+
+int main(int argc, char *argv[]){
+    std::cout << "Contour Detection with XXX" << std::endl;
+}
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by Angelo Gennuso on 20/11/23.
+//
+
+#ifndef GPU_HOUGHEDGEPROJECT_MAIN_H
+#define GPU_HOUGHEDGEPROJECT_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //GPU_HOUGHEDGEPROJECT_MAIN_H
