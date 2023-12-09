4fe26c1865a221aad26501594e9cbac48d86aa0c
Angelo Gennuso <gennusoangelo17@gmail.com>
2023-12-09 18:18:42 +0100

    Added timings on main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#include <chrono>
+
 
 
 void apply_filter(int kernel_size, int height, int width, uint8_t *output, uint8_t *input, float *kernel)
@@ -73,6 +75,7 @@ void non_maximum_suppression(int height, int width, uint8_t *suppr_mag, uint8_t 
 			suppr_mag[i*width + j] = 0;
      	}
  	}
+
 
 	for(int i = 1; i < height-1; i++)
  	{
@@ -156,6 +159,19 @@ void hysteresis(int height, int width, uint8_t *pixel_classification){
  	}
 }
 
+void measure_time(bool start, FILE* file_times, std::string name){
+	static std::chrono::system_clock::time_point start_time;
+	static std::chrono::system_clock::time_point end_time;
+	
+	if(start){
+		start_time = std::chrono::system_clock::now();
+	} else {
+		end_time = std::chrono::system_clock::now();
+		std::chrono::duration<double> duration = end_time - start_time;
+		fprintf(file_times, "%s: %f \n", name.c_str(), duration.count());
+	}
+}
+
 
 
 int main(int argc, char *argv[])
@@ -164,8 +180,11 @@ int main(int argc, char *argv[])
 
 	// does this datatype lead to bank conflicts? yes but we can't do anything about it
     
-	auto img_fname = argc>=2 ? argv[1] : "esa.png";
+	auto img_fname = argc>=2 ? argv[1] : "image.png";
 
+	system("mkdir -p output");
+	auto file_times = fopen("./output/times.txt", "a");
+	
 	uint8_t* rgb_image = stbi_load(img_fname, &width, &height, &bpp, 3);
 
     std::cout<<"image: "<<img_fname<<std::endl;
@@ -174,7 +193,10 @@ int main(int argc, char *argv[])
 	// Convert to greyscale
     uint8_t* grey_image;
     grey_image = (uint8_t*)malloc(width*height);
+
+	measure_time(true, file_times, "convert_to_greyscale");
 	convert_to_greyscale(height, width, rgb_image, grey_image);
+	measure_time(false, file_times, "convert_to_greyscale");
 	stbi_image_free(rgb_image);
 
 	// Apply Gaussian filtering
@@ -183,10 +205,12 @@ int main(int argc, char *argv[])
 	uint8_t* gaussian_image;
     gaussian_image = (uint8_t*)malloc(width*height);
 
+	measure_time(true, file_times, "apply_gaussian_filter");
 	apply_filter(kernel_size, height, width, gaussian_image, grey_image, gaussian_filter);
+	measure_time(false, file_times, "apply_gaussian_filter");
 
 	stbi_image_free(grey_image);
-	stbi_write_png("image_gaussian.png", width, height, 1, gaussian_image, width);
+	stbi_write_png("./output/0_image_gaussian.png", width, height, 1, gaussian_image, width);
 	
 	// Apply Sobel filtering
 	float sobel_h[kernel_size*kernel_size] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
@@ -196,12 +220,14 @@ int main(int argc, char *argv[])
 	uint8_t* sobel_image_v;
     sobel_image_v = (uint8_t*)malloc(width*height);
 
+	measure_time(true, file_times, "apply_sobel_filters");
 	apply_filter(kernel_size, height, width, sobel_image_h, gaussian_image, sobel_h);
 	apply_filter(kernel_size, height, width, sobel_image_v, gaussian_image, sobel_v);
-   
+	measure_time(false, file_times, "apply_sobel_filters");
+
     stbi_image_free(gaussian_image);
-	stbi_write_png("image_sobel_h.png", width, height, 1, sobel_image_h, width);
-	stbi_write_png("image_sobel_v.png", width, height, 1, sobel_image_v, width);
+	stbi_write_png("./output/1_image_sobel_h.png", width, height, 1, sobel_image_h, width);
+	stbi_write_png("./output/1_image_sobel_v.png", width, height, 1, sobel_image_v, width);
 
 	// Calculate magnitude and gradient direction
     float* gradient_direction;
@@ -209,18 +235,22 @@ int main(int argc, char *argv[])
 	uint8_t* magnitude;
     magnitude = (uint8_t*)malloc(width*height);
 
+	measure_time(true, file_times, "compute_magnitude_and_gradient");
 	compute_magnitude_and_gradient(height, width, sobel_image_h, sobel_image_v, magnitude, gradient_direction);
+	measure_time(false, file_times, "compute_magnitude_and_gradient");
 
 	stbi_image_free(sobel_image_v);
 	stbi_image_free(sobel_image_h);
-	stbi_write_png("gradient_direction.png", width, height, 1, gradient_direction, width);
-	stbi_write_png("magnitude.png", width, height, 1, magnitude, width);
+	stbi_write_png("./output/2_gradient_direction.png", width, height, 1, gradient_direction, width);
+	stbi_write_png("./output/2_magnitude.png", width, height, 1, magnitude, width);
 
 	// Non-maximum suppression
 	uint8_t* suppr_mag;
     suppr_mag = (uint8_t*)malloc(width*height);
 
+	measure_time(true, file_times, "non_maximum_suppression");
 	non_maximum_suppression(height, width, suppr_mag, magnitude, gradient_direction);
+	measure_time(false, file_times, "non_maximum_suppression");
 
 	double max = *std::max_element(magnitude, magnitude + width*height);
 
@@ -228,19 +258,23 @@ int main(int argc, char *argv[])
 
 	stbi_image_free(magnitude);
 	stbi_image_free(gradient_direction);
-	stbi_write_png("nonmax_suppr.png", width, height, 1, suppr_mag, width);
+	stbi_write_png("./output/3_nonmax_suppr.png", width, height, 1, suppr_mag, width);
 
 	// classify pixels as strong, weak or non-relevant
 	uint8_t* pixel_classification;
     pixel_classification = (uint8_t*)malloc(width*height);
 	
+	measure_time(true, file_times, "double_threshold");
 	double_threshold(height, width, pixel_classification, suppr_mag);
+	measure_time(false, file_times, "double_threshold");
 
-	stbi_write_png("thresholded.png", width, height, 1, pixel_classification, width);
+	stbi_write_png("./output/4_thresholded.png", width, height, 1, pixel_classification, width);
 
+	measure_time(true, file_times, "hysteresis");
 	hysteresis(height, width, pixel_classification);
+	measure_time(false, file_times, "hysteresis");
 
-	stbi_write_png("hysteresis.png", width, height, 1, pixel_classification, width);
+	stbi_write_png("./output/5_hysteresis.png", width, height, 1, pixel_classification, width);
 
     return 0;
 }
